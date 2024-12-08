a1dac8ba84ed0e1e0c71f2626d972c2cfeb6e4b3
unknown <sacunia@fi.uba.ar>
2024-12-08 08:45:06 -0300

    Fixed non synthesizable line by adding function for non trivial alignment

--- rtl/fp_add_sub.vhd
+++ rtl/fp_add_sub.vhd
@@ -2,7 +2,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-entity fp_sum_subs is
+entity fp_add_sub is
     generic (
         NE: natural := 8;
         NF: natural := 23
@@ -10,21 +10,18 @@ entity fp_sum_subs is
     port (
         rst:      in std_logic;
         clk:      in std_logic;
-        sum_sub:  in std_logic;
+        add_sub:  in std_logic;
         x:        in std_logic_vector(NF+NE downto 0);
         y:        in std_logic_vector(NF+NE downto 0);
         z:        out std_logic_vector(NF+NE downto 0)
     );
-end fp_sum_subs;
+end fp_add_sub;
 
-architecture behavioral of fp_sum_subs is
-    constant ZERO_E:        std_logic_vector(NE-1 downto 0) := (others => '0');
-    constant ZERO_F:        std_logic_vector(NF-1 downto 0) := (others => '0');
-    constant E_MIN:         signed(NE downto 0) := to_signed(0, NE+1);
-    constant E_MAX:         signed(NE downto 0) := to_signed(2**(NE)-2, NE+1);
+architecture behavioral of fp_add_sub is
+    constant E_MAX  :       signed(NE downto 0) := to_signed(2**(NE-1)-1, NE+1);
     signal x_reg :          std_logic_vector(NF+NE downto 0);
     signal y_reg :          std_logic_vector(NF+NE downto 0);
-    signal sum_sub_reg:     std_logic:= '0';
+    signal add_sub_reg:     std_logic:= '0';
     signal sx :             std_logic:= '0';
     signal ex :             signed(NE-1 downto 0);
     signal fx :             std_logic_vector(NF-1 downto 0);
@@ -59,6 +56,14 @@ architecture behavioral of fp_sum_subs is
     signal ez_p :           signed(NE-1 downto 0);
     signal ez_not_sat :     signed(NE downto 0);
     
+    function not_trivial_alignment (smaller_mantissa: unsigned; diff: integer) return unsigned is
+        variable mantissa_nt_a: unsigned(2**NE+NF-1 downto 0):= (others =>'0');
+        begin
+            for i in smaller_mantissa'range loop 
+                mantissa_nt_a(i +2**NE -diff - 1):= smaller_mantissa(i);
+            end loop;
+            return mantissa_nt_a;
+    end function not_trivial_alignment;
 
     function first_one_index (a_signal: std_logic_vector) return natural is
         variable index		 : natural ;
@@ -82,11 +87,11 @@ begin
         if rst = '1' then
             x_reg <= (others => '0');
             y_reg <= (others => '0');
-            sum_sub_reg <= '0';
+            add_sub_reg <= '0';
         elsif rising_edge(clk) then
             x_reg <= x;
             y_reg <= y;
-            sum_sub_reg <= sum_sub;
+            add_sub_reg <= add_sub;
         end if;
     end process;
 
@@ -113,64 +118,61 @@ begin
     my_p <= my when exp_diff(NE) = '0' else mx;
        
     mx_p_t_a <= mx_p & to_unsigned(0, 2**NE-1);
-                    
-    my_p_nt_a <= to_unsigned(0, to_integer(abs_exp_diff)) & my_p &
-                     to_unsigned(0, 2**NE-1- to_integer(abs_exp_diff));
-
+               
+    my_p_nt_a <= not_trivial_alignment(my_p, to_integer(abs_exp_diff));  
     ca2_mx_p <= not('0' & mx_p_t_a) + 1;
     ca2_my_p <= not('0' & my_p_nt_a) + 1;
 
-    selector <= sum_sub_reg & sx & sy & exp_diff(NE);
+    selector <= add_sub_reg & sx & sy & exp_diff(NE);
     process (selector)
-    begin
-        
+    begin 
         case (selector) is
-            when "0000" =>  -- Suma, ambos positivos
+            when "0000" =>  
                 c_x <= '0';
                 c_y <= '0';
-            when "0001" =>  -- Suma, ambos positivos, swap
+            when "0001" =>  
                 c_x <= '0';
                 c_y <= '0';
-            when "0010" =>  -- Suma, primero positivo, segundo negativo
+            when "0010" =>  
                 c_x <= '0';
                 c_y <= '1';
-            when "0011" =>  -- Suma, primero positivo, segundo negativo, swap
+            when "0011" =>  
                 c_x <= '1';
                 c_y <= '0';
-            when "0100" =>  -- Suma, primero negativo, segundo positivo
+            when "0100" =>  
                 c_x <= '1';
                 c_y <= '0';
-            when "0101" =>  -- Suma, primero negativo, segundo positivo, swap
+            when "0101" =>  
                 c_x <= '0';
                 c_y <= '1';
-            when "0110" =>  -- Suma, primero negativo, segundo negativo
+            when "0110" =>  
                 c_x <= '1';
                 c_y <= '1';
-            when "0111" =>  -- Suma, primero negativo, segundo negativo, swap
+            when "0111" =>  
                 c_x <= '1';
                 c_y <= '1';
-            when "1000" =>  -- Resta, ambos positivos
+            when "1000" =>  
                 c_x <= '0';
                 c_y <= '1';
-            when "1001" =>  -- Resta, ambos positivos, swap
+            when "1001" =>  
                 c_x <= '1';
                 c_y <= '0';
-            when "1010" =>  -- Resta, primero positivo, segundo negativo
+            when "1010" =>  
                 c_x <= '0';
                 c_y <= '0';
-            when "1011" =>  -- Resta, primero positivo, segundo negativo, swap
+            when "1011" =>  
                 c_x <= '0';
                 c_y <= '0';
-            when "1100" =>  -- Resta, primero negativo, segundo positivo
+            when "1100" =>  
                 c_x <= '1';
                 c_y <= '1';
-            when "1101" =>  -- Resta, primero negativo, segundo positivo, swap
+            when "1101" =>  
                 c_x <= '1';
                 c_y <= '1';
-            when "1110" =>  -- Resta, primero negativo, segundo negativo
+            when "1110" =>  
                 c_x <= '1';
                 c_y <= '0';
-            when "1111" =>  -- Resta, primero negativo, segundo negativo, swap
+            when "1111" =>  
                 c_x <= '0';
                 c_y <= '1';
             when others =>
@@ -191,23 +193,25 @@ begin
              
     mz_shifted <=  shift_left(mz, shift);
     
-    fz_p <= mz_shifted(2**NE + NF - 1 downto 2**NE);-- viejo
+    fz_p <= mz_shifted(2**NE + NF - 1 downto 2**NE);
 
     ez_p <= ex when exp_diff(NE) = '0' else ey;
     
-    ez_not_sat <= resize(ez_p, NE+1) - resize(to_signed(shift, NE+1), NE+1);
+    ez_not_sat <= resize(ez_p, NE+1) - resize(to_signed(shift, NE+1), NE+1) + 1;
+        
+    sz <= sy when fx = (fx'range => '0') and ex = (ex'range => '0') and add_sub_reg = '0' else
+          not(sy) when fx = (fx'range => '0') and ex = (ex'range => '0') and add_sub_reg = '1' else
+          sx when fy = (fy'range => '0') and ey = (fx'range => '0') else
+          sum_m_ext(2**NE + NF);
 
     ez <= ey when (fx = (fx'range => '0') and ex = (ex'range => '0')) else
           ex when (fy = (fy'range => '0') and ey = (ey'range => '0')) else
-          ez_not_sat(NE-1 downto 0) +1;
-             
-    sz <= sy when fx = (fx'range => '0') and ex = (ex'range => '0') and sum_sub_reg = '0' else
-          not(sy) when fx = (fx'range => '0') and ex = (ex'range => '0') and sum_sub_reg = '1' else
-          sx when fy = (fy'range => '0') and ey = (fx'range => '0') else
-          sum_m_ext(2**NE + NF);
-         
+          E_MAX(NE-1 downto 0) when (ez_not_sat(NE-1 downto 0) > E_MAX) else
+          ez_not_sat(NE-1 downto 0);
+                     
     fz <= fy when (fx = (fx'range => '0') and ex = (ex'range => '0')) else
           fx when  (fy = (fy'range => '0') and ey = (fx'range => '0')) else
+          (others => '1') when (ez_not_sat(NE-1 downto 0) > E_MAX) else
           std_logic_vector(fz_p);
              
     process(clk,rst)
